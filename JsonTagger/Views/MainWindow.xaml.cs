bbc09fcb2638881cb5b9d417a4ae888c680db6de
bdotz <bdotz@outlook.com>
2026-06-04 17:51:19 +0330

    Modernizing UI

--- JsonTagger/Views/MainWindow.xaml.cs
+++ JsonTagger/Views/MainWindow.xaml.cs
@@ -1,14 +1,43 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace JsonTagger.Views;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        StateChanged += (_, _) =>
+            MaxRestoreBtn.Content = WindowState == WindowState.Maximized
+                ? "\uE923"
+                : "\uE922";
     }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            WindowState = WindowState == WindowState.Maximized
+                ? WindowState.Normal
+                : WindowState.Maximized;
+        }
+        else
+        {
+            DragMove();
+        }
+    }
+
+    private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        => WindowState = WindowState.Minimized;
+
+    private void MaxRestoreBtn_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
+    }
+
+    private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        => Close();
 }
